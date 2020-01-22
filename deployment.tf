6adabd4f7df5ec985145e55872761ee607c5d499
Christopher LeJeune <gnurulz@gmail.com>
2020-01-22 21:56:06 +0000

    added data portion

--- deployment.tf
+++ deployment.tf
@@ -18,5 +18,22 @@ provider "aws" {
 
 # data
 
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*]
+  }
+  
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
